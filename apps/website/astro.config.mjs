cfd35dbfb04faec9366adf6f96c7efa90ad3ca3f
JAM <ontoneio.mm@gmail.com>
2023-11-07 00:08:30 -0800

    add image tools dep

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -4,10 +4,12 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/static';
 import tsconfigPaths from 'vite-tsconfig-paths'
 import UnoCSS from 'unocss/astro'
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   root: '../../',
+  site: `${process.env.SITE_URL}`,
   output: 'static',
   adapter: vercel(),
   plugins: [
@@ -26,6 +28,7 @@ export default defineConfig({
       apiVersion: 'v2023-03-07',
       token: `${process.env.ASTRO_VIEWER_AUTH}`
     }),
+    astroImageTools
   ],
   server:{
     port: import.meta.env.NODE_ENV !== 'production' ? 3000 : import.meta.env.PORT
